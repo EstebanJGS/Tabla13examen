0d5d168243ef2810ba0e5a3173ac0fd966efe6f3
EstebanJGS <guerrero.saucedo.esteban@gmail.com>
2026-03-12 08:16:06 -0600

    first commit

---
+++ src/main/java/com/examen/tabla13/Tabla13examenPracticoApplication.java
@@ -0,0 +1,13 @@
+package com.examen.tabla13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tabla13examenPracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tabla13examenPracticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/tabla13/Tabla13examenPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.tabla13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tabla13examenPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
